e2300564870011c1409de95d0337bc4f9efda2ca
gkhnclpbnci <gkhnclpbnci@gmail.com>
2017-05-06 00:24:38 +0300

    Puan durumu eklendi , uygulama simgesi yüklendi

--- app/src/main/java/com/gkhnclpbnci/altay/pages/LeagueTablesFragment.java
+++ app/src/main/java/com/gkhnclpbnci/altay/pages/LeagueTablesFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.gkhnclpbnci.altay.MainActivity;
 import com.gkhnclpbnci.altay.R;
@@ -15,6 +16,7 @@ import com.gkhnclpbnci.altay.R;
  * A simple {@link Fragment} subclass.
  */
 public class LeagueTablesFragment extends Fragment {
+    WebView puan;
 
     Activity titleChange;
     public LeagueTablesFragment() {
@@ -28,7 +30,11 @@ public class LeagueTablesFragment extends Fragment {
 
         // Inflate the layout for this fragment
         titleChange.setTitle("Puan Durumu");
+
         View rootView = inflater.inflate(R.layout.fragment_leaguetables, container, false);
+        puan=(WebView)rootView.findViewById(R.id.puan_durumu);
+        String html = "<iframe src=\"http://www.skyturks.com/2_3_lig_ekle/3_lig_1_grup_puan_durumu_ekle.asp\" scrolling=\"no\" frameborder=\"0\" marginwidth=\"0\" marginheight=\"0\" align=\"top\" width=\"385\" height=\"485\" name=\"I6\"></iframe>";
+        puan.loadData(html, "text/html", null);
 
         return rootView;
     }
